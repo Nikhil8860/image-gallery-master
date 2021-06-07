043c68a602943592a30b043ac9ca8354eb0af3ad
Nikhil Sharma <sharmanikhil8287@gmail.com>
2021-06-07 09:10:45 +0530

    Added about.html, add-blog.html, callback.html, footer.html, gallery.js, post.html, pincode.js, paytm.html, paytm_checksum.py and do some changes

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 import os
 from flask import Flask, request, render_template, send_from_directory, session, redirect, flash
+from flask_session import Session
 import json
 from flask_mysqldb import MySQL
-
+from datetime import timedelta
 import MySQLdb.cursors
+import requests
+import logging
+import datetime
+from paytm_checksum import generate_checksum, verify_checksum
 
 app = Flask(__name__)
 mysql = MySQL(app)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
+
 app.secret_key = 'super-secret-key'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'charan_dal'
-
+Session(app)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+MERCHANT_ID = "lNwrBZ12471380578241"
+MERCHANT_KEY = "QmfZKLHeBQmFd@sD"
+WEBSITE_NAME = "WEBSTAGING"
+# WEBSITE_NAME = "http://127.0.0.1:8080/"
+INDUSTRY_TYPE_ID = "Retail"
+# BASE_URL = "https://securegw.paytm.in"
+BASE_URL = "https://securegw-stage.paytm.in"
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 with open('config.json', 'r') as c:
@@ -35,10 +54,21 @@ def register():
         email = request.form['email']
         phone = request.form['phone']
         msg = request.form['msg']
+        dob = request.form['dob']
+        city = request.form['city']
+        pin_code = request.form['pincode']
+        district = request.form['district']
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        sql = 'INSERT INTO register (first_name, last_name, email, phone, msg ) VALUES (%s, %s, %s, %s, %s)'
-        val = (first_name, last_name, email, phone, msg)
+        check_sql = "select * from register where email=" + "'" + email + "'"
+        cursor.execute(check_sql)
+        data = cursor.fetchone()
+        if data:
+            flash("User already exist")
+            return render_template('register.html', msg=msg)
+        sql = 'INSERT INTO register (first_name, last_name, dob, city, pincode, district, email, phone, msg ) ' \
+              'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        val = (first_name, last_name, dob, city, pin_code, district, email, phone, msg)
 
         cursor.execute(sql, val)
         mysql.connection.commit()
@@ -47,6 +77,49 @@ def register():
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('register.html', msg=msg)
+
+
+@app.route('/about')
+def about():
+    try:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        sql = "Select * from blog"
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        return render_template('about.html', data=data)
+    except (ConnectionError, MySQLdb.OperationalError):
+        return "DB not Connected"
+
+
+@app.route("/about/<string:about_slug>", methods=['GET'])
+def post_route(about_slug):
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    sql = "Select * from blog where title=" + "'" + about_slug + "'"
+    print(sql)
+    cursor.execute(sql)
+    data = cursor.fetchone()
+    return render_template('post.html', post=data)
+
+
+@app.route('/blog', methods=['GET', 'POST'])
+def blog():
+    msg = ''
+    if request.method == 'POST':
+        title = request.form['title']
+        descr = request.form['descr']
+        print(title, descr, sep='--')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        sql = 'INSERT INTO blog (title, description) ' \
+              'VALUES (%s, %s)'
+        val = (title, descr)
+
+        cursor.execute(sql, val)
+        mysql.connection.commit()
+        msg = 'Post Added Successfully!'
+        flash(msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('add-blog.html', msg=msg)
 
 
 @app.route('/home')
@@ -99,6 +172,7 @@ def dashboard():
         if (username == 'admin' and userpass == params['admin_user']):
             # set the session variable
             session['user'] = username
+            session.permanent = True
             return redirect('/upload')
 
     return render_template('login.html', params=params)
@@ -147,6 +221,59 @@ def logout():
 
 def static_file_hash(filename):
     return int(os.stat(filename).st_mtime)
+
+
+@app.route("/payment")
+def payment():
+    amount = 11.07
+    transaction_data = {
+        "MID": MERCHANT_ID,
+        "WEBSITE": WEBSITE_NAME,
+        "INDUSTRY_TYPE_ID": INDUSTRY_TYPE_ID,
+        "ORDER_ID": str(datetime.datetime.now().timestamp()),
+        "CUST_ID": "007",
+        "TXN_AMOUNT": str(amount),
+        "CHANNEL_ID": "WEB",
+        "MOBILE_NO": "7777777777",
+        "EMAIL": "example@paytm.com",
+        "CALLBACK_URL": "http://127.0.0.1:8080/callback"
+    }
+
+    # Generate checksum hash
+    transaction_data["CHECKSUMHASH"] = generate_checksum(transaction_data, MERCHANT_KEY)
+
+    logging.info("Request params: {transaction_data}".format(transaction_data=transaction_data))
+
+    url = BASE_URL + '/theia/processTransaction'
+    return render_template("paytm.html", data=transaction_data, url=url)
+
+
+@app.route('/callback', methods=["GET", "POST"])
+def callback():
+    # log the callback response payload returned:
+    callback_response = request.form.to_dict()
+    logging.info("Transaction response: {callback_response}".format(callback_response=callback_response))
+
+    # verify callback response checksum:
+    checksum_verification_status = verify_checksum(callback_response, MERCHANT_KEY,
+                                                   callback_response.get("CHECKSUMHASH"))
+    logging.info("checksum_verification_status: {check_status}".format(check_status=checksum_verification_status))
+
+    # verify transaction status:
+    transaction_verify_payload = {
+        "MID": callback_response.get("MID"),
+        "ORDERID": callback_response.get("ORDERID"),
+        "CHECKSUMHASH": callback_response.get("CHECKSUMHASH")
+    }
+    url = BASE_URL + '/order/status'
+    verification_response = requests.post(url=url, json=transaction_verify_payload)
+    logging.info("Verification response: {verification_response}".format(
+        verification_response=verification_response.json()))
+
+    return render_template("callback.html",
+                           callback_response=callback_response,
+                           checksum_verification_status=checksum_verification_status,
+                           verification_response=verification_response.json())
 
 
 if __name__ == "__main__":
